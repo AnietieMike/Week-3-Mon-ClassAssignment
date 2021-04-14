6d465d1f18dbf3af3a9ad4daed2325c7d4bc88e2
decagon <anietieudoaka@gmail.com>
2021-04-14 15:06:33 +0100

    New changes

--- app/src/main/java/com/decagon/anietie/notification/MainActivity.kt
+++ app/src/main/java/com/decagon/anietie/notification/MainActivity.kt
@@ -30,21 +30,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Notification button
         notificationButton = findViewById(R.id.notification_button)
+        // Next activity button
         nextActivityButton = findViewById(R.id.next_button)
 
+
+        // Clicking the 'Next' button takes the user to the second activity displaying 'Inactive'
         nextActivityButton.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
             intent.putExtra("Inactive", "Inactive")
             startActivity(intent)
         }
 
+        // Clicking the Notification button calls the showNotification function
         notificationButton.setOnClickListener {
-            showNotification()
+            newNotification()
         }
     }
 
-    fun showNotification() {
+
+    // Create a new notification
+    fun newNotification() {
         val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
@@ -54,12 +61,16 @@ class MainActivity : AppCompatActivity() {
             channel.description = getString(R.string.channel_description)
             mNotificationManager.createNotificationChannel(channel)
         }
+
+        // The notification builder
         val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.mipmap.ic_launcher) // notification icon
                 .setContentTitle(getString(R.string.notification_title)) // title for notification
                 .setContentText(getString(R.string.notification_message))// message for notification
                 .setAutoCancel(true) // clear notification after click
 
+
+        // Passes the notification text to the Second Activity
         val intent = Intent(this, SecondActivity::class.java)
         intent.putExtra("Active", "Active")
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
